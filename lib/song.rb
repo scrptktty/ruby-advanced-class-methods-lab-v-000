ebf532232c58b74017fa16977470e082b45262d7
Kachel Wittig <kjwittig@gmail.com>
2018-04-02 15:33:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +9,37 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    # Yields the new instance to the block, and then returns new instance as well.
+    #self.new.tap --> barewords fall through to methods
+    new.tap { |it| it.save }
+  end
+
+  def new_by_name(name)
+
+  end
+
+  def create_by_name(name)
+
+  end
+
+  def find_or_create_by_name
+  end
+
+  def alphabetical
+    all.sort
+  end
+
+  def new_from_filename
+  end
+
+  def create_by_filename
+  end
+
+  def self.destroy_all
+    all.clear
   end
 
 end
