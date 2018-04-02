d2a313a6cc785236df02005d247a014c5c95693c
Kachel Wittig <kjwittig@gmail.com>
2018-04-02 16:09:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(find_name) || self.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
